da051b2861ea7d5cd586729cc6eb331f323363bf
Mitchell Hanberg <mitch@mitchellhanberg.com>
2026-02-27 10:32:59 -0500

    ci: make sure prefetch tools are in path

--- dev/checksums/fetchers.nix
+++ dev/checksums/fetchers.nix
@@ -1,11 +1,18 @@
 _: {
-  perSystem = {pkgs, ...}: {
+  perSystem = {pkgs, ...}: let
+    nix-prefetch-deps = pkgs.lib.makeBinPath [
+      pkgs.nix-prefetch-git
+      pkgs.nix
+    ];
+  in {
     packages = {
       nix-prefetch-elixir = pkgs.writeShellScriptBin "nix-prefetch-elixir" ''
+        export PATH="${nix-prefetch-deps}:$PATH"
         ${pkgs.nix-prefetch-github}/bin/nix-prefetch-github elixir-lang elixir --rev v''${1} | ${pkgs.jq}/bin/jq -r .hash
       '';
 
       nix-prefetch-otp = pkgs.writeShellScriptBin "nix-prefetch-otp" ''
+        export PATH="${nix-prefetch-deps}:$PATH"
         ${pkgs.nix-prefetch-github}/bin/nix-prefetch-github erlang otp --rev OTP-''${1} | ${pkgs.jq}/bin/jq -r .hash
       '';
     };
